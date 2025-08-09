0a210d7eb0ab470deb8a21e5075e84656543d96b
leducmanh0838 <leducmanh0838@gmail.com>
2025-08-09 12:15:54 +0700

    a

--- front-end/src/firebase/firebase.js
+++ front-end/src/firebase/firebase.js
@@ -4,13 +4,6 @@ import { getFirestore } from "firebase/firestore";
 // process.env.REACT_APP_GOOGLE_CLIENT_ID
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyC1runtSJmblXPYFtoVX1NGZcYLPOM8-bI",
-  // authDomain: "final-exam-web-system.firebaseapp.com",
-  // projectId: "final-exam-web-system",
-  // storageBucket: "final-exam-web-system.firebasestorage.app",
-  // messagingSenderId: "219822759312",
-  // appId: "1:219822759312:web:b97ef4673f436706066715",
-  // measurementId: "G-YF3HQL87QY"
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
